8221566176179c0d7d3903ae9e2e87fb907bced4
rajan23-git <pranavrajan568@gmail.com>
2022-12-06 02:18:19 +0000

    added lots of comments for documentation of routes and methods

--- flask_app.py
+++ flask_app.py
@@ -9,11 +9,12 @@ oregon_timezone = timezone('US/Pacific')
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+#this global boolean initializes the lock status to being locked
+#detected_motion_distance and detected_motion_timestamp have not been calculated yet
 unlock_boolean = "Lock"
 detected_motion_distance = "not calculated yet"
 detected_motion_timestamp = "not calculated yet"
-
+#using SQLAlchemy database to store data, such as passcodes and login attempts
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="pranavrajan568",
     password="SQLPassword143",
@@ -24,6 +25,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 database = SQLAlchemy(app)
+
+#this is the database model for the SQL PasscodeTable
+#the table contains the fields passcode_configured and timestamp
+#passcode configured is the passcode configured by the administrator and timestamp is the time it was configured
 class PasscodeTable(database.Model):
     __tablename__ = "PasscodeTable"
     id = database.Column(database.Integer, primary_key = True)
@@ -31,6 +36,9 @@ class PasscodeTable(database.Model):
     passcode_configured = database.Column(database.String(4096))
     timestamp = database.Column(database.String(4096))
 
+#SQL database model for Login_Attempt table
+#contains the fields passcode_entered, which is the passcode that the user entered on the ESP32 keypad
+#the field correct_boolean indicates whether the entered passcode matched the correct passcode, and timestamp is the time when passcode was entered
 class LoginAttemptTable(database.Model):
     __tablename__ = "LoginAttemptTable"
     id = database.Column(database.Integer, primary_key = True)
@@ -39,17 +47,21 @@ class LoginAttemptTable(database.Model):
     correct_boolean = database.Column(database.String(4096))
     timestamp = database.Column(database.String(4096))
 
+#this function returns the 5 most recent login attempts from the LoginAttempt table in the SQL database
 def getRecentLoginAttempts():
     login_attempts = database.session.query(LoginAttemptTable).all()
     table_size = len(login_attempts)
     recent_attempts = login_attempts[(table_size - 5) : table_size]
     return recent_attempts
 
-
+#this route returns the unlocked status of the lock
+#this unlocked status is fetched by the frontend ESP32 through a GET request, in order to lock or unlock the smart keypad
 @app.route('/FetchUnlockedStatus')
 def fetchUnlockedStatus():
     return unlock_boolean
 
+#this route has a form, where Admin can choose to lock or unlock the smart keypad.
+#the form makes a POST request to this route, and stores the unlock_boolean in a global variable.
 @app.route('/UnlockDoor',methods = ["GET","POST"])
 def unlockDoor():
     if(request.method == "POST"):
@@ -58,7 +70,9 @@ def unlockDoor():
     return render_template('UnlockDoor.html',unlocked_status = unlock_boolean)
 
 
-
+#this route receives the distance between the user and the ultrasonic sensor.
+#receives this distance through a POST request made on the frontend ESP32.
+#stores and timestamps this distance in a global variable
 @app.route('/<doorbell_distance>',methods = ["GET","POST"])
 def receiveMotion(doorbell_distance):
     if(request.method == "POST"):
@@ -72,7 +86,8 @@ def receiveMotion(doorbell_distance):
 
     return "default return value"
 
-
+#this route returns the current configured passcode
+#this passcode is then fetched by the frontend ESP32 for validating passcodes entered by the user
 @app.route('/FetchConfiguredPasscode')
 def fetchConfiguredPasscode():
     all_entries = database.session.query(PasscodeTable).all()
@@ -80,6 +95,9 @@ def fetchConfiguredPasscode():
     latest_passcode = all_entries[num_entries - 1]
     return str(latest_passcode.passcode_configured)
 
+#this route receives login attempts from the user, on the ESP32 keypad
+#receives the entered passcode, and correct boolean through a POST Request
+#adds this login attempt to the LoginAttempt database table
 @app.route('/<entered_passcode>/<correct_boolean>',methods = ["GET","POST"] )
 def receiveLoginAttempt(entered_passcode,correct_boolean):
     if(request.method == "POST"):
@@ -91,7 +109,8 @@ def receiveLoginAttempt(entered_passcode,correct_boolean):
 
     return  "default return value"
 
-
+#Displays the 5 most recent login attempts on an html webpage, and displays recent motion detected at the doorbell
+#doorbell motion refers to detected distance between the user and the ultrasonic sensor
 @app.route('/LoginAttempts',methods = ["GET","POST"])
 def loginAttempts():
 
@@ -99,6 +118,8 @@ def loginAttempts():
     print(getRecentLoginAttempts())
     return render_template('LoginAttempts.html',login_attempt_array = getRecentLoginAttempts(), motion_distance = detected_motion_distance , motion_timestamp = detected_motion_timestamp )
 
+#this route contains a form, where the administrator can enter the configured passcode, that will be used in the IOT smart keypad
+#this form makes a POST request to this route, and adds the configured passcode to the PasscodeTable, in the SQL database
 @app.route('/ConfigurePasscode',methods = ["GET","POST"])
 def configurePasscode():
     passcode_string = "passcode not configured yet"
